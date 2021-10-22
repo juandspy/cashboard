58b8f0b1a969196ece603f72d1fcba89017abffc
Juan Diaz Suarez <jdiazsua@redhat.com>
2021-10-22 13:28:53 +0200

    add a function to calculate asset delta for last days

--- reader/reader.py
+++ reader/reader.py
@@ -1,21 +1,30 @@
 import piecash
 from typing import List
+from datetime import datetime
+import pytz
 
 
 class CashStore:
-    def __init__(self, book_path):
-        self.book =  piecash.open_book(
-            book_path, 
-            readonly=True, 
-            do_backup=False)
+    def __init__(self, book_path=None, book=None):
+        if ( book == None ) & ( book_path != None ): 
+            self.book =  piecash.open_book(
+                book_path, 
+                readonly=True, 
+                do_backup=False)
+        elif book != None:
+            self.book = book
+        else:
+            raise ValueError("Need book_path or book")
         
         self.accounts = get_accounts(self.book)
 
 class asset:
-    def __init__(self, name, balance, last_month_balance):
+    def __init__(self, name, balance, last_month_balance, account=None):
         self.name = name 
         self.balance = balance 
         self.last_mont_balance = last_month_balance
+
+        self.account = account
 
 def get_accounts(book: piecash.core.book.Book) -> List[asset]:
     accounts = []
@@ -26,11 +35,20 @@ def get_accounts(book: piecash.core.book.Book) -> List[asset]:
 def get_assets(book: piecash.core.book.Book) -> List[asset]:
     assets = []
     for acc in book.accounts(type="ASSET").children:
-        print(type(acc))
         assets.append(
             asset(
                 name=acc.name, 
                 balance=acc.get_balance(natural_sign=False), 
-                last_month_balance=0)
+                last_month_balance=0,
+                account=acc)
         )
     return assets
+
+def get_asset_delta(book: piecash.core.book.Book, asset: piecash.core.account.Account, days: int) -> float:
+    delta = 0
+    now = datetime.now().replace(tzinfo=pytz.utc)
+
+    for split in asset.splits:
+        if split.transaction.enter_date > now.replace(day=days):
+            delta += split.quantity
+    return delta
--- reader/reader_test.py
+++ reader/reader_test.py
@@ -4,10 +4,11 @@ from piecash.core.factories import single_transaction
 from decimal import Decimal
 from datetime import datetime, timedelta
 
-from reader import CashStore, get_assets
+from reader import CashStore, get_assets, get_asset_delta
 
 
 TRANSACTION_VALUE = Decimal(100)
+NUMBER_OF_TRANSACTONS = 3
 BANK_INITIAL_VALUE = 1000
 BANK_ACCOUNT_NAME = "Account 1 bank sub account"
 CASH_INITIAL_VALUE = 100
@@ -15,6 +16,11 @@ CASH_ACCOUNT_NAME = "Account 1 cash sub account"
 
 mock_book = piecash.create_book(currency="EUR")
 EUR = mock_book.commodities.get(mnemonic="EUR")
+
+opening_balance = piecash.Account(name="Opening Balance",
+    type="EQUITY",
+    parent=mock_book.root_account,
+    commodity=EUR)
 
 acc = piecash.Account(name="Account 1",
     type="ASSET",
@@ -39,14 +45,22 @@ cash = piecash.Account(name=CASH_ACCOUNT_NAME,
     code="CASH1",)
 
 now = datetime.now()
-this_month = now.month
-last_month = now.month - 1 or 12
-transaction_date = now.replace(month=last_month)
 
+transaction_date = now.replace(month=now.month - 1 or 12)
+for (acc, balance) in zip ([bank, cash], [BANK_INITIAL_VALUE, CASH_INITIAL_VALUE]):
+    single_transaction(
+        post_date=transaction_date.date(),
+        enter_date=transaction_date,
+        description="a month ago balance",
+        value=balance,
+        from_account=opening_balance,
+        to_account=acc)
+
+transaction_date = now.replace(day=now.day - 1)
 single_transaction(
     post_date=transaction_date.date(),
     enter_date=transaction_date,
-    description="a month ago",
+    description="yesterday withdrawal from bank to cash",
     value=TRANSACTION_VALUE,
     from_account=bank,
     to_account=cash)
@@ -58,10 +72,17 @@ mock_store = CashStore(book=mock_book)
 
 
 def test_get_accounts():
-    assert len(mock_store.accounts) == 1
+    assert len(mock_store.accounts) == 2
 
 def test_get_assets():
     assets = get_assets(mock_store.book)
     assert len(assets) == 2
     assert "Account 1 bank sub account" == assets[0].name
+    assert BANK_INITIAL_VALUE - TRANSACTION_VALUE == assets[0].balance
     assert "Account 1 cash sub account" == assets[1].name
+    assert CASH_INITIAL_VALUE + TRANSACTION_VALUE == assets[1].balance
+
+def test_get_asset_delta():
+    assets = get_assets(mock_store.book)
+    assert get_asset_delta(mock_store.book, assets[0].account, 5) == -TRANSACTION_VALUE
+    assert get_asset_delta(mock_store.book, assets[1].account, 5) == TRANSACTION_VALUE
