ebead5e05ce0f8e8dd6edb9670a8f647f85d5628
Juan Diaz Suarez <jdiazsua@redhat.com>
2021-11-21 18:41:49 +0100

    fix depth thingy at cashstore_test

--- reader/cashstore_test.py
+++ reader/cashstore_test.py
@@ -17,10 +17,16 @@ def test_CashStore_accounts():
 def test_CashStore_assets():
     assert mock_store.assets == []
     mock_store.set_assets_depth(1)
-    assert len(mock_store.assets) == len([BANK_ACCOUNT_NAME, CASH_ACCOUNT_NAME])
+    assert len(mock_store.assets) == len([ASSETS_NAME])
+
     mock_store.set_assets_depth(2)
+    assert len(mock_store.assets) == len([BANK_ACCOUNT_NAME, CASH_ACCOUNT_NAME])
+
+    mock_store.set_assets_depth(3)
     assert len(mock_store.assets) == len([BANK_SUB_ACCOUNT_1_NAME, BANK_SUB_ACCOUNT_2_NAME, CASH_ACCOUNT_NAME])
 
 def test_CashStore_update_assets_splits():
     mock_store.set_assets_depth(1)
+    assert mock_store.assets[0].split_df.empty
+    mock_store.set_assets_depth(2)
     assert not mock_store.assets[0].split_df.empty
