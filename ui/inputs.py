808eaad7759eb3281ebdc2a1a9d4120e9c257e41
Juan Diaz Suarez <jdiazsua@redhat.com>
2022-08-23 12:04:06 +0200

    move inputs to sidebar

--- ui/inputs.py
+++ ui/inputs.py
@@ -1,6 +1,6 @@
 """UI inputs.
 """
-from streamlit import columns as streamlit_columns
+import streamlit as st
 
 
 def setup_inputs() -> (int, bool, int):
@@ -10,24 +10,26 @@ def setup_inputs() -> (int, bool, int):
         int, bool, int: depth; whether to show the delta as
         percentage or not; and the regression degree.
     """
-    input_columns = streamlit_columns(3)
+    sidebar = st.sidebar
 
-    depth = input_columns[0].number_input(
+    sidebar.markdown("""---""")
+
+    depth = sidebar.number_input(
         'Insert desired depth',
         value=3,
         step=1)
 
-    add_months_to_pred = input_columns[1].number_input(
+    add_months_to_pred = sidebar.number_input(
         "Add months to the regression",
         value=0,
         step=1,
         min_value=0,
         max_value=None)
 
-    delta_percentage = input_columns[1].checkbox(
+    delta_percentage = sidebar.checkbox(
         "Delta in percentage")
 
-    reg_degree = input_columns[2].number_input(
+    reg_degree = sidebar.number_input(
         "Regression degree",
         value=1,
         step=1,
