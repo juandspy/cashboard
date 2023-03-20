8f8ebed87957f6794e141656272422a1046fd353
Juan Díaz <juandiazsuarez0@gmail.com>
2023-03-20 15:13:42 +0100

    fix SQL_QUERY__CUSTOM_START
    the query wasn't ready for mixed possitive/negative expenses

--- ui/const.py
+++ ui/const.py
@@ -13,25 +13,27 @@ SELECTBOX_CUSTOM_QUERY = "Run a custom query"
 
 
 SQL_QUERY__CUSTOM_START = """
-SELECT SUM(value_num)/100 FROM (
-
-SELECT joined.value_num, t.description, t.post_date, t.enter_date
-FROM
-    (
-        SELECT to_accounts.tx_guid, from_accounts.account_guid AS from_account, to_accounts.account_guid AS to_account, to_accounts.value_num
-        FROM
-        (
-        SELECT tx_guid, account_guid, value_num
-        FROM splits
-        WHERE value_num > 0
-        ) AS to_accounts,
-        (
-        SELECT tx_guid, account_guid, value_num
-        FROM splits
-        WHERE value_num < 0
-        ) AS from_accounts
-        WHERE to_accounts.tx_guid = from_accounts.tx_guid
-    ) AS joined, transactions as t
-    WHERE t.guid = joined.tx_guid AND t.description LIKE "CUSTOM_START%"
-)
+SELECT 
+  SUM(value_num) 
+FROM 
+  (
+    SELECT 
+      DISTINCT s1.value_num / 100. AS value_num, 
+      t1.description, 
+      t1.post_date, 
+      a1.name AS from_account, 
+      a1.account_type AS from_account_type, 
+      a2.name AS to_account, 
+      a2.account_type AS to_account_type 
+    from 
+      splits s1 
+      INNER JOIN splits s2 ON s1.tx_guid = s2.tx_guid 
+      INNER JOIN transactions t1 ON t1.guid = s2.tx_guid 
+      INNER JOIN accounts a1 ON s1.account_guid = a1.guid 
+      INNER JOIN accounts a2 ON s2.account_guid = a2.guid 
+    where 
+      a1.name != a2.name 
+      AND a2.account_type == "EXPENSE"
+      AND t1.description LIKE "CUSTOM_START%" 
+  )
 """
